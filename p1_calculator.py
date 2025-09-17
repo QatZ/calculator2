59cc5906b0ba5a26239f4be6301a6cb257f52d05
Vladimir Katasonov <katasonovvladimir@gmail.com>
2025-09-17 21:14:12 +0200

    fix: correct operation input type from int to str (#29)
    closes #29

--- p1_calculator.py
+++ p1_calculator.py
@@ -1,20 +1,25 @@
-def calculator():
-    no_1 = int(input("enter your first number"))
-    operation = int(input("enter  arithmetic operation  like +,-,*,/"))
-    no_2 = int(input("enter your  second number"))
+"""The calculator which calculates the calculations."""
 
-    if operation =='+':
+
+def calculator() -> None:
+    """Simple calculator that reads two numbers and prints results."""
+    no_1 = float(input("enter your first number"))
+    operation = input("enter  arithmetic operation  like +,-,*,/")
+    no_2 = float(input("enter your  second number"))
+
+    if operation == "+":
         print(f"the sum of {no_1} + {no_2} are {no_1 + no_2}")
-    elif  operation =='-':
+    elif operation == "-":
         print(f"the diff of {no_1}  - {no_2} are {no_1 - no_2}")
-    elif operation =='*':
+    elif operation == "*":
         print(f"the product of {no_1} * {no_2} are {no_1 * no_2}")
-    elif  operation =='/':
+    elif operation == "/":
         if no_2 == 0:
             print("it gives infinity as it is not divided by zero")
         else:
             print(f"the sum of {no_1}  / {no_2} are {no_1 / no_2}")
     else:
         print("invalid output")
+
 
 calculator()
